460fc3b758f48f0bd4a831ac40b466c951105288
Aladino Borges <eualadino@gmail.com>
2021-07-21 06:53:44 -0300

    Configuração das Routes e Switch.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import { Switch, Route } from 'react-router-dom';
 import AppProvider from './context/AppProvider';
 import Splash from './pages/SplashScreen';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
-    <AppProvider>
-      <div className="App">
-        <Splash />
-      </div>
-    </AppProvider>
+    <div className="App">
+      <AppProvider>
+        <Switch>
+          <Route exact path="/" component={ Splash } />
+          <Route exact path="/home" component={ Home } />
+        </Switch>
+      </AppProvider>
+    </div>
   );
 }
 
