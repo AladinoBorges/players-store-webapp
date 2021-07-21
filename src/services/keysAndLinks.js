378ef1baf5a2c721a9a280770002fc81e438b98b
Aladino Borges <eualadino@gmail.com>
2021-07-21 03:23:39 -0300

    Refactora o link da requisição API.

--- src/services/keysAndLinks.js
+++ src/services/keysAndLinks.js
@@ -1,3 +1,5 @@
-export const apiKey = 'af194e99a15a416d95b45fb57c7182f7';
+// const apiKey = 'af194e99a15a416d95b45fb57c7182f7';
 
-export const gamesURL = `https://api.rawg.io/api/platforms?key=${apiKey}`;
+const gamesURL = `https://api.rawg.io/api/games?key=af194e99a15a416d95b45fb57c7182f7`;
+
+export default gamesURL;
