9e28f64cfc5eb786bdc942bf45900a19f21bba94
Aladino Borges <eualadino@gmail.com>
2021-07-23 19:54:53 -0300

    App.js: configuração final das rotas.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Switch, Route } from 'react-router-dom';
 import AppProvider from './context/AppProvider';
-import Splash from './pages/SplashScreen';
+import Details from './pages/Details';
 import Home from './pages/Home';
 import './App.css';
 
@@ -9,8 +9,8 @@ function App() {
     <div className="App">
       <AppProvider>
         <Switch>
-          <Route exact path="/" component={ Splash } />
-          <Route exact path="/home" component={ Home } />
+          <Route exact path="/" component={ Home } />
+          <Route exact path="/game/:id" component={ Details } />
         </Switch>
       </AppProvider>
     </div>
