d7a08499c7c8ed04fd0e3c68e0b977e437b1e708
Aladino Borges <eualadino@gmail.com>
2021-07-23 19:56:59 -0300

    Lógica final homepage.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,56 +1,86 @@
 import { useContext, useState, useEffect } from 'react';
+import axios from 'axios';
 
 import AppContext from '../context/AppContext';
 import Header from '../components/Header';
-import Footer from '../components/Footer';
 import NavBar from '../components/Navbar';
-import Aside from '../components/Aside';
-import genericGetFromStorage from '../services/getFromLocalStorage';
+import GameCard from '../components/GameListCard';
+import Footer from '../components/Footer';
+import Loading from '../components/Loading';
 
 function Home() {
   const { globalState, setGlobalState } = useContext(AppContext);
-  const [cardsInfo, setCardsInfo] = useState([]);
-  const { listOfGames } = globalState;
+  const [listToRender, setListToRender] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    // COMPONENT DID MOUNT?
-    useEffect(() => {
-      const gamesList = genericGetFromStorage('gamesList').results;
-      const info = !(listOfGames.length) ? gamesList : listOfGames;
+  const { listOfGames, next } = globalState;
+  const loadTime = 4500; // 4500
 
-      setCardsInfo([...info]);
-    }, []);
+  const requestAPI = async (url) => {
+    try {
+      const api = await axios.get(url)
+      .then(({ data }) => data);
 
+      const { next, results } = api;
+
+      setGlobalState({ ...globalState, next, listOfGames: [...results] });
+    
+    } catch (error) { console.log(error) }
+  };
+
+  const handleLoading = ((status) => {
+    setTimeout(() => setLoading(status), loadTime);
+  })
+  
+  // COMPONENT DID MOUNT?
+  useEffect(() => {
+    listToRender.length === 0 && requestAPI(next);
+    handleLoading(false);
+  }, []);
+
+  // COMPONENT DID UPDATE?
+  useEffect(() => {
+    const updatedList = (listToRender.length > 0)
+      ? listToRender.concat(listOfGames)
+      : listOfGames;
+
+    setListToRender(updatedList);
+  }, [listOfGames]);
+
+  const handleClick = () => {
+    requestAPI(next);
+  };
+  
   return (
-    <div>
-      <Header />
-      <NavBar />
-      <section className="cards_container">
-        { cardsInfo.map(({ background_image, name, slug, released, rating, platforms }, index) => (
-          <div key={ index } className="cards">
-            <div className="card_img_container">
-              <img className="card_img" src={ background_image } alt={ `${slug} cover` } />
-            </div>
-            <div className="card_text_container">
-              <h2>{ name }</h2>
-              <p>{ `Lançamento: ${released.replace(/-/g, ' - ')}` }</p>
-              <p>{ `Avaliação: ${rating}` }</p>
-              <div className="platforms_container">
-                <h4>
-                  PLATAFORMAS:
-                </h4>
-                <p>
-                  { platforms.map(({platform: { name, slug }}) => (
-                    <span key={ slug }>{ `${name} ` }</span>
-                  ))}
-                </p>
-              </div>
-            </div>
-          </div>
-        ))}
-      </section>
-      <Aside />
-      <Footer />
-    </div>
+    <>
+      {loading ? <Loading />
+        : (
+        <div>
+          <Header page={'HOME'} />
+          <NavBar />
+          <section className="cards_container">
+            {listToRender.map(({ id, background_image, name, slug, released, rating, platforms }, index) => (
+              <GameCard
+                name={ name }
+                id={ id }
+                index={ index }
+                background_image={ background_image }
+                slug={ slug }
+                released={ released }
+                rating={ rating }
+                platforms={ platforms }
+              />))}
+          <button
+            type="button"
+            onClick={ handleClick }
+          >
+            MORE GAMES
+          </button>
+          </section>
+          <Footer />
+        </div>
+      )}
+    </>
   );
 }
 
